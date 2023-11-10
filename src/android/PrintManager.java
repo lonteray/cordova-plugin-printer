3cce2ada78bca07a7d0b9dbcc481b296873b8680
Elie Habka <ehabka@koein.com>
2023-11-10 12:57:16 +0200

    f

--- src/android/PrintManager.java
+++ src/android/PrintManager.java
@@ -8,7 +8,7 @@ import android.print.PrintDocumentAdapter;
 import android.print.PrintJob;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.print.PrintHelper;
+import android.support.v4.print.PrintHelper;
 import android.webkit.CookieManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
--- src/android/PrintProxy.java
+++ src/android/PrintProxy.java
@@ -7,7 +7,7 @@ import android.print.PageRange;
 import android.print.PrintAttributes;
 import android.print.PrintDocumentAdapter;
 import androidx.annotation.NonNull;
-import androidx.print.PrintHelper;
+import android.support.v4.print.PrintHelper;
 
 /**
  * Simple delegate class to have access to the onFinish method.
